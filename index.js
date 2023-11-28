19d61b83dc5abc7007cd4b5a46ba128d08641015
monicavf <monicavf87@gmail.com>
2023-11-28 09:42:47 -0300

    código desafio

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hey")
+let nome = "monica"
+let nivel = 5000
+let xp;
+
+if (nivel <= 1000){
+    xp = "Ferro"
+} else if (nivel >= 1001 && nivel <= 2000){
+    xp = "Bronze"
+}else if (nivel >= 2001 && nivel <= 5000){
+    xp = "Prata";
+}else if (nivel >= 6001 && nivel <= 7000){
+    xp = "Ouro";
+}else if (nivel >= 7001 && nivel <= 8000){
+    xp = "Platina";
+}else if (nivel >= 8001 && nivel <= 9000){
+    xp = "Ascendente";
+}else if (nivel >= 9001 && nivel <= 10000){
+    xp = "Imortal";
+}else{
+    xp ="Radiante";
+}
+
+console.log("O Herói de nome " + nome + " está no nível " + xp + ".")
